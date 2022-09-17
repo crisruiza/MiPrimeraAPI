3955a423209cf7659ebe54d9ea29b8873b3f1670
Cristina Ruiz <cristinaruizald@outlook.com>
2022-09-17 10:31:46 -0500

    API Picsum

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 console.log("Entro al main.js");
+
+const randomImage = () => {
+  console.log("Entró a la función randomImage");
+  // Función para traer la API
+  fetch("https://picsum.photos/200/300")
+    // Trae la data de la API
+    .then((data) => {
+      console.log(data);
+      // Tomamos la imagen por ID y reescribirmos la URL que trae la API
+      document.getElementById("imgRandom").src = data.url;
+    })
+    .catch((error) => {
+      console.log("Ocurrió un error");
+    });
+};
